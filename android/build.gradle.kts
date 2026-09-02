551b3085e932c0c3848254f918032db404ae23cb
klofmanju221-alt <klofmanju221@gmail.com>
2026-09-02 15:45:29 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
 allprojects {
     repositories {
         google()
@@ -9,12 +13,24 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: Directory =
+        newBuildDir.dir(project.name)
+
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    plugins.withId("com.android.library") {
+        if (project.name == "agora_rtc_engine") {
+            extensions.configure<LibraryExtension> {
+                compileSdk = 35
+            }
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
