e15a01f561b30e89a7ffc5b8a4d84000285ccf23
klofmanju221-alt <klofmanju221@gmail.com>
2026-09-03 11:34:07 +0530

    Update agora_config.dart

--- lib/agora_config.dart
+++ lib/agora_config.dart
@@ -1,1 +1,17 @@
+class AgoraConfig {
+  // Agora App ID
+  static const String appId =
+      '67442996296a4c45a180b6ac3e6495ed';
 
+  // TEST ONLY
+  // Token GitHub Actions Secret ಮೂಲಕ build ಸಮಯದಲ್ಲಿ ಬರುತ್ತದೆ.
+  static const String tempToken =
+      String.fromEnvironment(
+    'AGORA_TEMP_TOKEN',
+    defaultValue: '',
+  );
+
+  // Temporary Token ಈಗ ಈ channelಗಾಗಿ generate ಮಾಡಲಾಗಿದೆ.
+  static const String testChannel =
+      'mchat_test';
+}
