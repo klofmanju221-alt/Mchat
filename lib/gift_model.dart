92106f87bdbbc86cff2989a2798210d47b5e3b65
klofmanju221-alt <klofmanju221@gmail.com>
2026-09-05 21:29:48 +0530

    Update gift_model.dart

--- lib/gift_model.dart
+++ lib/gift_model.dart
@@ -1,1 +1,35 @@
+class GiftModel {
+  final String id;
+  final String name;
+  final String emoji;
+  final int coinCost;
+  final bool enabled;
 
+  const GiftModel({
+    required this.id,
+    required this.name,
+    required this.emoji,
+    required this.coinCost,
+    this.enabled = true,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'emoji': emoji,
+      'coinCost': coinCost,
+      'enabled': enabled,
+    };
+  }
+
+  factory GiftModel.fromMap(Map<String, dynamic> map) {
+    return GiftModel(
+      id: map['id']?.toString() ?? '',
+      name: map['name']?.toString() ?? '',
+      emoji: map['emoji']?.toString() ?? '🎁',
+      coinCost: (map['coinCost'] as num?)?.toInt() ?? 0,
+      enabled: map['enabled'] as bool? ?? true,
+    );
+  }
+}
